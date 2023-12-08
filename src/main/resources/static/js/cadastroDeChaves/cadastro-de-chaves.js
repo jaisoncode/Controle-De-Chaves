a2b84db08a27ef451ba0f598b85181067e9094a4
Jaison <jaisoncode@gmail.com>
2023-12-08 15:09:07 -0300

    add busca vazia

--- src/main/resources/static/js/cadastroDeChaves/cadastro-de-chaves.js
+++ src/main/resources/static/js/cadastroDeChaves/cadastro-de-chaves.js
@@ -108,7 +108,7 @@ function exibirChaves(data) {
             language: {
                 url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/pt-BR.json',
                 "search": ""
-            }
+            }        
         });
 
         table.on('draw.dt', function () {   
